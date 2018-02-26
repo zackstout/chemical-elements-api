4e2d3da5b70bbb9c39c434f097fe61c9d7942444
Zack Stout <zackstout@gmail.com>
2018-02-25 20:34:06 -0600

    almost done with map of orbitals

--- server/public/client.js
+++ server/public/client.js
@@ -1,10 +1,21 @@
 
 var allChems = [];
 var can2;
-var boxWidth, boxHeight;
+var boxWidth, boxHeight, ctx;
 
+var can1Width, can1Height, can1BoxWidth, can1BoxHeight;
 
 function setup() {
+
+  var can1 = document.getElementById('orbitals');
+  // console.log(can1.width);
+  ctx = can1.getContext('2d');
+  can1Width = can1.width;
+  can1Height = can1.height;
+  console.log(can1.width);
+  can1BoxWidth = can1Width / 7;
+  can1BoxHeight = can1Height / 118;
+
   console.log('hi p5');
   can2 = createCanvas(900, 400);
   background(201);
@@ -20,21 +31,85 @@ function setup() {
     console.log(res);
     for (var i=0; i < res.length; i++) {
       var r = res[i];
-      // console.log(r.image);
-      // var img = '<img src="' + r.image + '">';
-      // console.log(img);
-
-
 
 
       // $('table').append('<tr style=background-color:' + r.color + '>' + '<td>' + r.num + '</td>' + '<td>' + r.name + '</td>' + '<td>' + r.sym + '</td>' + '<td>' + r.origin + '</td>' + '<td>' + r.melt + '</td>' + '<td>' + r.boil + '</td>' + '<td>' + r.c + '</td>' + '<td>' + r.x + '</td>' + '<td>' + r.weight + '</td>' + '<td>' + r.density + '</td>' + '<td>' + r.group + '</td>' + '<td>' + r.period + '</td>' + '<td><img src="pics/' + r.name + '_spectrum_visible.png' + '"></td>' + '</tr>');
 
 
+      // how do we do this without concatenation again...?:
+      if (r.num != 80) {
+        var shellNums = r.shells.split(', ');
+        $('#orbitals').append('<p id="' + r.num + '"></p>');
 
+        shellNums.forEach((n, index) => {
+          var int = parseInt(n);
+          // console.log(int, index);
 
+          switch(index) {
+            case 0:
+            // console.log(can1BoxWidth);
+            ctx.fillStyle = 'red';
+            ctx.fillRect(0, (r.num - 1) * can1BoxHeight, can1BoxWidth, can1BoxHeight);
+            ctx.fillStyle = 'black';
+            ctx.font = '22px Arial';
+            ctx.fillText('hi', 0, (r.num - 1) * can1BoxHeight);
+            break;
 
-      // saveFile(r.image);
-      // window.downloadFile(r.image);
+            case 1:
+            // while (int > 0) {
+            //   $('#orbitals').append()
+            // }
+            ctx.fillStyle = 'orange';
+            ctx.fillRect(can1BoxWidth, (r.num - 1) * can1BoxHeight, can1BoxWidth, can1BoxHeight);
+            ctx.fillStyle = 'black';
+            ctx.font = '22px Arial';
+            ctx.fillText('hi', can1BoxWidth, (r.num - 1) * can1BoxHeight);
+            // console.log(1);
+            break;
+
+            case 2:
+            ctx.fillStyle = 'yellow';
+            ctx.fillRect(can1BoxWidth * 2, (r.num - 1) * can1BoxHeight, can1BoxWidth, can1BoxHeight);
+            ctx.fillStyle = 'black';
+            ctx.font = '22px Arial';
+            ctx.fillText('hi', can1BoxWidth * 2, (r.num - 1) * can1BoxHeight);
+            break;
+
+            case 3:
+            ctx.fillStyle = 'green';
+            ctx.fillRect(can1BoxWidth * 3, (r.num - 1) * can1BoxHeight, can1BoxWidth, can1BoxHeight);
+            ctx.fillStyle = 'black';
+            ctx.font = '22px Arial';
+            ctx.fillText('hi', can1BoxWidth * 3, (r.num - 1) * can1BoxHeight);
+            break;
+
+            case 4:
+            ctx.fillStyle = 'blue';
+            ctx.fillRect(can1BoxWidth * 4, (r.num - 1) * can1BoxHeight, can1BoxWidth, can1BoxHeight);
+            ctx.fillStyle = 'black';
+            ctx.font = '22px Arial';
+            ctx.fillText('hi', can1BoxWidth *4, (r.num - 1) * can1BoxHeight);
+            break;
+
+            case 5:
+            ctx.fillStyle = 'violet';
+            ctx.fillRect(can1BoxWidth *5, (r.num - 1) * can1BoxHeight, can1BoxWidth, can1BoxHeight);
+            ctx.fillStyle = 'black';
+            ctx.font = '22px Arial';
+            ctx.fillText('hi', can1BoxWidth*5, (r.num - 1) * can1BoxHeight);
+            break;
+
+            case 6:
+            ctx.fillStyle = 'indigo';
+            ctx.fillRect(can1BoxWidth*6, (r.num - 1) * can1BoxHeight, can1BoxWidth, can1BoxHeight);
+            ctx.fillStyle = 'black';
+            ctx.font = '22px Arial';
+            ctx.fillText('hi', can1BoxWidth*6, (r.num - 1) * can1BoxHeight);            break;
+          }
+        });
+        // $('#orbitals').append('<p>' + r.shells + '</p>');
+      }
+
 
       r.xCoord = (r.group - 1) * boxWidth;
       r.yCoord = (r.period - 1) * boxHeight;
@@ -50,6 +125,9 @@ function setup() {
         text(r.num, r.xCoord, r.yCoord + 10);
         textSize(20);
         text(r.sym, r.xCoord + 15, r.yCoord + 35);
+      } else {
+        // handle the 2 weird rows:
+
       }
 
       allChems.push(r);
@@ -70,7 +148,7 @@ function mouseClicked() {
       }).done((res) => {
         console.log(res);
         $('#holder').empty();
-        $('#holder').append('<ul>' + '<li>Name: ' + res.el_name + '</li>' + '<li>Origin: ' + res.origin + '</li>' + '<li>Melt: ' + res.melt + '</li>' + '<li>Boil: ' + res.boil + '</li>' + '<li>Density: ' + res.density + '</li>' + '<li>Electron Config: ' + res.config + '</li>' + '<li>Specific Heat: ' + res.c + '</li>' + '<li>Electronegativity: ' + res.x + '</li>' + '<li>Shells: ' + res.shells + '</li>' + '<li>STP: ' + res.stp + '</li>' + '</ul>');
+        $('#holder').append('<ul>' + '<li>Name: ' + res.el_name + '</li>' + '<li>Origin: ' + res.origin + '</li>' + '<li>Melt: ' + res.melt + '</li>' + '<li>Boil: ' + res.boil + '</li>' + '<li>Density: ' + res.density + '</li>' + '<li>Electron Config: ' + res.config + '</li>' + '<li>Specific Heat: ' + res.c + '</li>' + '<li>Electronegativity: ' + res.x + '</li>' + '<li>Shells: ' + res.shells + '</li>' + '<li>Group: ' + res.group_num + '</li>' + '</ul>');
       }).catch((err) => {
         console.log(err);
       });
@@ -79,10 +157,10 @@ function mouseClicked() {
   });
 }
 
-function draw() {
-
-}
-
+// function draw() {
+//
+// }
+//
 // $(document).ready(() => {
 //
 // });
